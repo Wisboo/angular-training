26a2cbf882d62136b4972c6290ba008c44a8b511
Angel Ortiz <aandresortiz@gmail.com>
2016-06-28 12:09:01 -0400

    base files

---
+++ src/app/app.routes.js
@@ -0,0 +1,20 @@
+angular.module('wisboo').config([
+  '$stateProvider', '$urlRouterProvider', '$locationProvider',
+  function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise( ($injector) => {
+      $injector.get('$state').go('404');
+    });
+
+    $stateProvider
+      .state('404', {
+        url: '/not-found',
+        views: {
+          main: {
+            templateUrl: '../app/layouts/error/404.html'
+          }
+        }
+      });
+
+    $locationProvider.html5Mode(true);
+  }
+]);
---
+++ vendorJs.js
@@ -0,0 +1,4 @@
+module.exports = [
+  'angular/angular.js',
+  'angular-ui-router/release/angular-ui-router.js'
+];
