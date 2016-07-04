638288b7d16c98550304367e6305c8a78e2c7700
Juan Silva <juanzymchuk@users.noreply.github.com>
2016-07-04 02:13:59 -0300

    Revert "Revert "Card edit profile""

--- src/app/components/users/profile-ctrl.js
+++ src/app/components/users/profile-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('wisboo').controller(
-  'ProfileController', ['User', function (User) {
+  'ProfileController', ['User', '$translate', function (User, $translate) {
     const self = this;
 
     User.checkout().then(function (user) {
@@ -16,12 +16,39 @@ angular.module('wisboo').controller(
     };
 
     self.saveChanges = function (user) {
+      self.savingErrorMsg = undefined;
       User.edit(user).then(
         function () {
           self.editing = false;
           self.oldData = undefined;
         },
-        function () {}
+        function (resp) {
+          if (resp.data.code === 202) {
+            $translate('USERNAME_TAKEN_ERROR').then(function (text) {
+              self.savingErrorMsg = text;
+            });
+          } else {
+            $translate('GENERAL_SAVING_ERROR').then(function (text) {
+              self.savingErrorMsg = text;
+            });
+          }
+        }
+      );
+    };
+
+    self.changePassword = function (newPassword) {
+      self.user.password = newPassword;
+      self.savingPassErrorMsg = undefined;
+      User.edit(self.user).then(
+        function () {
+          self.newPassword = '';
+          self.passwordConfirm = '';
+        },
+        function () {
+          $translate('GENERAL_SAVING_ERROR').then(function (text) {
+            self.savingPassErrorMsg = text;
+          });
+        }
       );
     };
 
